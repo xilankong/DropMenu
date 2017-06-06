00146dabf16fb141b61cd8302f9fb4e1a8ff8240
young.huang <young.huang@jfz.com>
2017-06-06 17:04:20 +0800

    Initial Commit

---
+++ DropMenu/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DropMenu
+//
+//  Created by yanghuang on 2017/6/6.
+//  Copyright © 2017年 com.yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DropMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  DropMenu
+//
+//  Created by yanghuang on 2017/6/6.
+//  Copyright © 2017年 com.yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
